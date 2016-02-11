bd5c0e3dc2efa925c781cbe6dc93b621517c4001
j-curtis <jcurtis2@u.rochester.edu>
2016-02-11 13:48:02 -0500

    Added header file for four vector class.

---
+++ FourVector.h
@@ -0,0 +1,25 @@
+//This is a header file for the class for four vectors
+//Jonathan Curtis
+//02/11/16
+
+#include <iostream>
+#include <fstream>
+
+#ifndef FOURVECTOR_H
+#define FOURVECTOR_H
+
+class FourVector{
+protected:
+	double t;	//the time component (x0)
+	double x;	//the x component of the vector
+	double y; 	//the y component
+	double z;	//the z component 
+
+public:
+	FourVector(double, double, double, double);	//This creates a four-vector given components
+	//components are given as 
+	//t x y z 
+
+	double operator*(FourVector& lh,FourVector& rh);	//The product 
+};
+#endif
